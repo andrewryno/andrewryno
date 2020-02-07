df54917677769bb81c1f79cd1275ea243d300fb2
Andrew Ryno <aryno@twilio.com>
2020-02-07 14:31:58 -0800

    Simplify and update with branding

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <header className="home-header">
+        <p>
+          Hi, I'm Andrew Ryno
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
