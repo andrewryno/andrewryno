c25d0f09b1e40761577ad87ee99a7ef7a7a17799
Andrew Ryno <ryno@ngrok.com>
2022-08-16 11:27:52 -0700

    Move to Hugo

--- src/Home.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Home from './Home';
-
-test('renders hi', () => {
-  const { getByText } = render(<Home />);
-  const linkElement = getByText(/Andrew Ryno/i);
-  expect(linkElement).toBeInTheDocument();
-});
