2088c1cd948381282a06382d11e5b2f1680b2941
Andrew Ryno <ryno@ngrok.com>
2024-02-01 14:03:10 -0800

    Upgrade astro and enable Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: "https://andrewryno.com",
   integrations: [tailwind(), sitemap()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
